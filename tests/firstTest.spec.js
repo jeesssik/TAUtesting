1abbac6c604d2dadfdf33a3803c8ca2ced6e77a6
jeesssik <jeesssik@gmail.com>
2021-05-31 14:59:41 -0300

    testing on get

--- tests/firstTest.spec.js
+++ tests/firstTest.spec.js
@@ -1,4 +1,4 @@
-const express =require('express');
+/*const express =require('express');
 const request =require('supertest');
 const expect = require('chai').expect;
 
@@ -23,3 +23,39 @@ describe('First test', () =>{
       .expect(200, "done");
   });
 })
+
+console.log("**************** TEST 1 ********************")
+
+
+*/
+
+/**** get testing ****/
+
+const app =require('../src/app');
+const request =require('supertest');
+const expect = require('chai').expect;
+
+
+
+describe('get requests', () =>{
+    it('get course id', () =>{
+        request(app)
+        .get('/course/1')
+        .end((err,res) =>{
+            expect(res.body.id).to.be.equal('1');
+        });
+    });
+
+    //localhost/course?name=mocha
+
+    it('get query param name', (done) =>{
+        request(app)
+        .get('/course')
+        .query({'name' : 'mocha'})
+        .expect(200, { id: '1', name: 'mocha'}, done);
+    })
+
+});
+
+
+console.log("**************** TEST 2 ********************")
